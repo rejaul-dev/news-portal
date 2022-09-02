50c9fc37847a19ceb868b788ec325e0ca971e46c
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-09-03 05:20:20 +0800

    added navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('Hello JS');
+const loadAllCategory = async () => {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+    displayAllNewsCategory(data.data.news_category);
+};
+
+
+const displayAllNewsCategory = category => {
+    const navMenu = document.getElementById("nav-menu");
+    category.forEach(category => {
+        const ul = document.createElement('li');
+        ul.innerHTML = `
+        <li>${category.category_name}</li>
+        `;
+        navMenu.appendChild(ul);
+    });
+};
+
+
+loadAllCategory();
+
