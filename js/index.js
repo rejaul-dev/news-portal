2f0a2dab3ecf427431ed72a0d26d2120b1121a70
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-09-04 19:16:49 +0800

    added model content

--- js/index.js
+++ js/index.js
@@ -87,8 +87,11 @@ const displayNewsDetails = news => {
   modalTitle.innerText = news.title;
   const newsDetails = document.getElementById('news-details');
   newsDetails.innerHTML = `
-  <p>Author Name: ${news.author.name}</p>
+  <img class="rounded xl" style="height:50px; weight:50px;" src="${news.author.img}" alt="">
+  <p>Author Name: ${news.author.name ? news.author.name : 'author name not found' }</p>
   <p>Publish Date: ${news.author.published_date}</p>
+  <p class="fw-bold">Total View: ${news.total_view ? news.total_view :'no view yet'}</p>
+
   
   
   `
