db2e8c14fbae191e1ec3155c8f12e5817d56d915
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-09-04 17:15:56 +0800

    added card content

--- js/index.js
+++ js/index.js
@@ -2,35 +2,42 @@ const loadCategories = async () => {
   const response = await fetch(
     "https://openapi.programming-hero.com/api/news/categories"
   );
-  const data = await response.json();
-  displayCategories(data.data.news_category);
+  try {
+    const data = await response.json();
+    displayCategories(data.data.news_category); 
+  }
+  catch (error) {
+    console.loh(error);
+  }
+  
+
 };
 
 const displayCategories = (categories) => {
   const menu = document.getElementById("all-category");
   categories.forEach((item) => {
-    // console.log(item);
-    const ul = document.createElement("ul");
-    // ul.classList.add("nav");
+    const ul = document.createElement("li");
     ul.innerHTML = `
-          <li>
-        <a onclick="loadCategoryDetails('${item.category_id}')" href="#">${item.category_name}</a>
-        </li>
+      <li class="nav-item">
+      <a class="nav-link active" onclick="loadCategoryDetails('${item.category_id}')" aria-current="page" href="#">${item.category_name}</a>
+      </li>
     `;
     menu.appendChild(ul);
   });
 };
 
-{
-  /* <li><a onclick="loadCategoryDetails('${item.category_id}') ${item.category_name}></a></li> */
-}
-
 const loadCategoryDetails = async (category_id) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/news/category/${category_id}`
   );
-  const data = await response.json();
-  displayCategoryDetails(data.data);
+  try {
+    const data = await response.json();
+    displayCategoryDetails(data.data);
+  }
+  catch {
+    console.log(error);
+  }
+  
 };
 
 const displayCategoryDetails = async (details) => {
@@ -39,22 +46,19 @@ const displayCategoryDetails = async (details) => {
   details.forEach((details) => {
     const detailDiv = document.createElement("div");
     detailDiv.innerHTML = `
-        <div class="card card-side shadow-xl p-8">
-            <img src="${details.thumbnail_url}" alt="Movie" />
+       <div class="col">
+          <div class="card p-4">
+            <img src="${details.thumbnail_url}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h2 class="card-title">${details.title}</h2>
-              <p>${details.details}</p>
-              <div class="card-actions justify-end">
-              <div class=" rounded-full">
-                <button class="btn btn-primary">Show More</button>
-              </div>
+              <h5 class="card-title fw-bold">${details.title.slice(0, 50)}...</h5>
+              <p class="card-text">${details.details.slice(0, 120)}...</p>
             </div>
           </div>
-        
+        </div>
         `;
     cardContainer.appendChild(detailDiv);
   });
 };
 
-loadCategoryDetails('01');
+loadCategoryDetails('08');
 loadCategories();
