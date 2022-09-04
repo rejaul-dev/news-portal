2f36a2f9ca4d5d5c3924f0cd1bf55f847b672bf3
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-09-04 14:35:33 +0800

    added footer

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
 const loadCategories = async () => {
-      const response = await fetch(
-        "https://openapi.programming-hero.com/api/news/categories"
-    );
-    const data = await response.json();
-    displayCategories(data.data.news_category);
-    
-}
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
+  const data = await response.json();
+  displayCategories(data.data.news_category);
+};
 
 const displayCategories = (categories) => {
-    const menu = document.getElementById("all-category");
-    categories.forEach(item => {
-        // console.log(item);
-        const ul = document.createElement("ul");
-        ul.classList.add("nav");
-        ul.innerHTML = `
-        <li class="nav-item">
-        <a class="nav-link" onclick="loadCategoryDetails('${item.category_id}')" href="#">${item.category_name}</a>
+  const menu = document.getElementById("all-category");
+  categories.forEach((item) => {
+    // console.log(item);
+    const ul = document.createElement("ul");
+    // ul.classList.add("nav");
+    ul.innerHTML = `
+          <li>
+        <a onclick="loadCategoryDetails('${item.category_id}')" href="#">${item.category_name}</a>
         </li>
-    `;  
+    `;
     menu.appendChild(ul);
-        
-    });
+  });
+};
+
+{
+  /* <li><a onclick="loadCategoryDetails('${item.category_id}') ${item.category_name}></a></li> */
 }
 
 const loadCategoryDetails = async (category_id) => {
@@ -31,81 +33,28 @@ const loadCategoryDetails = async (category_id) => {
   displayCategoryDetails(data.data);
 };
 
-
 const displayCategoryDetails = async (details) => {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.textContent = '';
-    details.forEach(details => {
-        const detailDiv = document.createElement('div');
-        detailDiv.innerHTML = `
-        <h1>Hello</h1>
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.textContent = "";
+  details.forEach((details) => {
+    const detailDiv = document.createElement("div");
+    detailDiv.innerHTML = `
+        <div class="card card-side shadow-xl p-8">
+            <img src="${details.thumbnail_url}" alt="Movie" />
+            <div class="card-body">
+              <h2 class="card-title">${details.title}</h2>
+              <p>${details.details}</p>
+              <div class="card-actions justify-end">
+              <div class=" rounded-full">
+                <button class="btn btn-primary">Show More</button>
+              </div>
+            </div>
+          </div>
         
-        `
-        cardContainer.appendChild(detailDiv);
-    })
+        `;
+    cardContainer.appendChild(detailDiv);
+  });
+};
 
-}
-
-
-// loadCategoryDetails();
+loadCategoryDetails('01');
 loadCategories();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const setAllMenu = async () => {
-//     const data = await loadAllCategory();
-
-//     const menu = document.getElementById("all-category");
-    
-//     for (const categories of data) {
-
-//         // console.log(categories.category_name);
-
-    //     const ul = document.createElement("ul");
-    //     ul.classList.add('nav');
-    //     ul.innerHTML = `
-    //     <li class="nav-item">
-    //     <a class="nav-link" onclick="loadAllCategory('${categories.category_name.category_id}')"  href="#">${categories.category_name}</a>
-    //     </li>
-    
-    
-    // `;
-//         menu.appendChild(ul)
-//     }
-    
-
-   
-// }
-
-// setAllMenu(); 
-// loadAllCategory();
-
-
